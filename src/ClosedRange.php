e61b625bb1d583bf89d874ed140cc6afc56628a5
apple-yagi <applesryu@gmail.com>
2022-10-04 15:10:13 +0900

    下端点と上端点を任意の数にできるように修正

--- src/ClosedRange.php
+++ src/ClosedRange.php
@@ -4,8 +4,18 @@ namespace TddWorkshop;
 
 class ClosedRange
 {
-    public int $lower = 3;
-    public int $upper = 7;
+    public int $lower;
+    public int $upper;
+
+    /**
+     * @param int $lower
+     * @param int $upper
+     */
+    public function __construct(int $lower, int $upper)
+    {
+        $this->upper = $upper;
+        $this->lower = $lower;
+    }
 
     public static function hello(): string
     {
--- tests/ClosedRangeTest.php
+++ tests/ClosedRangeTest.php
@@ -9,16 +9,21 @@ use TddWorkshop\ClosedRange;
 
 class ClosedRangeTest extends TestCase
 {
+    /** @var int ハッピーパスの下端点 */
+    const HappyLower = 3;
+    /** @var int ハッピーパスの上端点 */
+    const HappyUpper = 7;
+
     private ClosedRange $closedRange;
 
     public function setUp(): void
     {
-        $this->closedRange = new ClosedRange();
+        $this->closedRange = new ClosedRange(self::HappyLower, self::HappyUpper);
     }
 
     public function test整数閉区間は下端点と上端点を持つこと()
     {
-        $this->assertEquals(3, $this->closedRange->lower);
-        $this->assertEquals(7, $this->closedRange->upper);
+        $this->assertEquals(self::HappyLower, $this->closedRange->lower);
+        $this->assertEquals(self::HappyUpper, $this->closedRange->upper);
     }
 }
