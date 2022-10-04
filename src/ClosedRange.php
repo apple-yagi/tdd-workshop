16f93e321181d3cb57ca479dce7ea29eea3ef8fe
apple-yagi <applesryu@gmail.com>
2022-10-04 15:03:39 +0900

    test整数閉区間は下端点と上端点を持つこと

--- src/ClosedRange.php
+++ src/ClosedRange.php
@@ -4,6 +4,9 @@ namespace TddWorkshop;
 
 class ClosedRange
 {
+    public int $lower = 3;
+    public int $upper = 7;
+
     public static function hello(): string
     {
         return "hello world";
--- tests/ClosedRangeTest.php
+++ tests/ClosedRangeTest.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @noinspection NonAsciiCharacters */
+
 namespace MyTest;
 
 use PHPUnit\Framework\TestCase;
@@ -7,8 +9,16 @@ use TddWorkshop\ClosedRange;
 
 class ClosedRangeTest extends TestCase
 {
-    public function testSuccess()
+    private ClosedRange $closedRange;
+
+    public function setUp(): void
     {
-        $this->assertEquals("hello world", ClosedRange::hello());
+        $this->closedRange = new ClosedRange();
+    }
+
+    public function test整数閉区間は下端点と上端点を持つこと()
+    {
+        $this->assertEquals(3, $this->closedRange->lower);
+        $this->assertEquals(7, $this->closedRange->upper);
     }
 }
